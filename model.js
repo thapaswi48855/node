ba73213e858392c59ffa6707314f6d837729e88c
thapaswi48855 <thapsapps@gmail.com>
2024-04-21 10:11:05 +0530

    dt4/20

--- model.js
+++ model.js
@@ -49,7 +49,7 @@ const roleSchema = new mongoose.Schema({
 })
 
 const assigneByPermissions = new mongoose.Schema({
-    assignepermissionid:String,
+    assignepermissionid:BigInt,
     roleid: Object,
     rolename: String,
     moduleid: Object,
@@ -70,7 +70,7 @@ const assigneByPermissions = new mongoose.Schema({
 })
 
 const newUserSchema = new mongoose.Schema({
-    userid:String,
+    userid:BigInt,
     userRoleid: String,
     userName: String,
     userPhno: String,
